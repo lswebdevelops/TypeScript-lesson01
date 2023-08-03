16a8fec641b33d789e3d0ca2cecf392525ceb34c
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-08-03 13:27:05 -0300

    criating variables using objects

--- build/js/main.js
+++ build/js/main.js
@@ -1,19 +1,57 @@
 "use strict";
-let myName = 'Davi';
-let meaningOfLife;
-let isLoading;
-let test;
-myName = "Luciano";
-meaningOfLife = 34;
-isLoading = true;
-test = 43;
-test = "3dsfasdf";
-const sum = (a, b) => {
-    return a + b;
+let stringArr = ['one', 'hey', 'Dave']; // let stringArr: string[]
+let guitars = ['Strat', 'Les Paul', 5150]; // let guitars: (string | number)[]
+let mixedData = ['EVH', 1984, true]; // let mixedData: (string | number | boolean)[]
+stringArr[0] = "Luciano";
+stringArr.push("0");
+guitars.unshift(9);
+console.log(guitars); //[9, 'Strat', 'Les Paul', 5150]
+console.log(stringArr); //['Luciano', 'hey', 'Dave', '0']
+// stringArr = mixedData;
+mixedData = stringArr;
+// creating a any array type: 
+let test = [];
+// now a string array
+let bands = [];
+bands.push("Van Halen");
+// tuples
+// here you have to have the exactly order and length: 
+let myTuple = ['Dave', 42, true];
+let mixed = ['Dave', 42, true];
+mixed = myTuple; // no problem here 
+//myTuple = mixed // not assignable
+myTuple[0] = "Luciano";
+myTuple[2] = false;
+// myTuple[1]  ="2" // cannot be done
+console.log(myTuple); // ['Luciano', 42, false]
+//objects
+let myObj;
+myObj = [];
+console.log(typeof (myObj));
+myObj = {};
+const exampleObj = {
+    prop1: 'Davi',
+    prop2: true
 };
-// union type: 
-let album;
-let postId;
-let isActive; //  it could be 1 or 0, or true or false or even S and N or T and F
+exampleObj.prop2 = false;
+let evh = {
+    name: 'Eddie',
+    active: false,
+    albums: [1934, 234, "Ou32"],
+};
+let JP = {
+    name: 'Jimmy',
+    active: true,
+    albums: ["I", "II", 'IV'],
+};
+// https://www.youtube.com/watch?v=gieEQFIfgYc&t=17s 1:00:31
+//evh = JP // as soon  they have the same properties .. 
 // 
-let re = /\w+/g;
+//one property in an object can also be optional > use ?:
+//    active?: boolean,
+// using it in a function 
+console.clear();
+const greetGuitarist = (guitarist) => {
+    return `Hello ${guitarist.name}!`;
+};
+console.log(greetGuitarist(JP));
--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,103 @@
-let myName:string = 'Davi'
-
-let meaningOfLife: number;
-let isLoading: boolean;
-let test: any;
+let stringArr = ['one', 'hey', 'Dave']; // let stringArr: string[]
+let guitars = ['Strat', 'Les Paul', 5150]; // let guitars: (string | number)[]
+let mixedData = ['EVH', 1984, true]; // let mixedData: (string | number | boolean)[]
 
 
 
-myName = "Luciano";
-meaningOfLife = 34;
-isLoading = true;
-test = 43;
-test = "3dsfasdf"
+
+ stringArr[0] = "Luciano";
+
+stringArr.push("0")
+
+guitars.unshift(9)
+
+console.log(guitars);//[9, 'Strat', 'Les Paul', 5150]
+console.log(stringArr); //['Luciano', 'hey', 'Dave', '0']
 
 
 
-const sum =(a:number,b:string) => {
-    return a + b
+
+// stringArr = mixedData;
+mixedData = stringArr
+
+
+// creating a any array type: 
+
+let test = [];
+
+// now a string array
+let bands: string[] = []
+
+bands.push("Van Halen")
+
+// tuples
+// here you have to have the exactly order and length: 
+let myTuple: [string, number , boolean] = ['Dave', 42, true]
+
+let mixed  = ['Dave', 42, true]
+
+mixed = myTuple // no problem here 
+
+//myTuple = mixed // not assignable
+
+myTuple[0] = "Luciano"
+myTuple[2] = false
+// myTuple[1]  ="2" // cannot be done
+
+console.log(myTuple);// ['Luciano', 42, false]
+
+//objects
+let myObj: object;
+myObj = [];
+console.log(typeof(myObj));
+
+myObj = {};
+
+const exampleObj = {
+    prop1: 'Davi',
+    prop2: true
 }
-// union type: 
-let album : string | number;
-let postId: string | number;
-let isActive: number | boolean | string; //  it could be 1 or 0, or true or false or even S and N or T and F
-// 
-let re : RegExp = /\w+/g;
 
+exampleObj.prop2 = false;
+// exampleObj.prop2 = "Luciano";// can't cause it is boolean 
+
+
+type Guitarist = {
+     // could use interface Guitarist { >>> use interface for classes(is better for classes)
+        // name....
+    //  }
+    name: string,
+    active?: boolean,
+    //one property in an object can also be optional > use ?:
+//    active?: boolean,
+
+    albums: (string | number )[],
+
+}
+let evh: Guitarist = {
+    name: 'Eddie',
+    active: false,
+    albums: [1934, 234, "Ou32"],
+}
+
+let JP: Guitarist = {
+    name: 'Jimmy',
+    active: true,
+    albums: ["I", "II", 'IV'],
+}
+// https://www.youtube.com/watch?v=gieEQFIfgYc&t=17s 1:00:31
+//evh = JP // as soon  they have the same properties .. 
+
+// 
+
+  //one property in an object can also be optional > use ?:
+//    active?: boolean,
+// using it in a function 
+console.clear();
+
+const greetGuitarist = (guitarist: Guitarist) => {
+    return `Hello ${guitarist.name}!`;
+}
+
+console.log(greetGuitarist(JP));
 
