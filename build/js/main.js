d5661840c9262601be6b6cea5f68af3bb4de6b7b
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-08-03 10:31:50 -0300

    adding types

---
+++ build/js/main.js
@@ -0,0 +1,15 @@
+"use strict";
+let username = 'Davi';
+console.log(username);
+// let a: number  = 12;
+// let b: number = 6
+// let c :number = 2
+// console.log(a / b);
+// console.log(c * b);
+// console.log( b + b);
+let a = 12;
+let b = 6;
+let c = 2;
+console.log(a / b);
+console.log(c * b);
+console.log(b + b);
---
+++ src/main.ts
@@ -0,0 +1,22 @@
+let username = 'Davi'
+console.log(username);
+
+
+// let a: number  = 12;
+// let b: number = 6
+// let c :number = 2
+
+// console.log(a / b);
+
+// console.log(c * b);
+// console.log( b + b);
+
+
+let a:number = 12;
+let b:number = 6
+let c:number = 2
+
+console.log(a / b);
+
+console.log(c * b);
+console.log( b + b);
