31d06d722255a130b21c02aa3cea353e3341a03a
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-08-03 13:54:40 -0300

    enums

--- build/js/main.js
+++ build/js/main.js
@@ -1,7 +1,7 @@
 "use strict";
-let stringArr = ['one', 'hey', 'Dave']; // let stringArr: string[]
-let guitars = ['Strat', 'Les Paul', 5150]; // let guitars: (string | number)[]
-let mixedData = ['EVH', 1984, true]; // let mixedData: (string | number | boolean)[]
+let stringArr = ["one", "hey", "Dave"]; // let stringArr: string[]
+let guitars = ["Strat", "Les Paul", 5150]; // let guitars: (string | number)[]
+let mixedData = ["EVH", 1984, true]; // let mixedData: (string | number | boolean)[]
 stringArr[0] = "Luciano";
 stringArr.push("0");
 guitars.unshift(9);
@@ -9,16 +9,16 @@ console.log(guitars); //[9, 'Strat', 'Les Paul', 5150]
 console.log(stringArr); //['Luciano', 'hey', 'Dave', '0']
 // stringArr = mixedData;
 mixedData = stringArr;
-// creating a any array type: 
+// creating a any array type:
 let test = [];
 // now a string array
 let bands = [];
 bands.push("Van Halen");
 // tuples
-// here you have to have the exactly order and length: 
-let myTuple = ['Dave', 42, true];
-let mixed = ['Dave', 42, true];
-mixed = myTuple; // no problem here 
+// here you have to have the exactly order and length:
+let myTuple = ["Dave", 42, true];
+let mixed = ["Dave", 42, true];
+mixed = myTuple; // no problem here
 //myTuple = mixed // not assignable
 myTuple[0] = "Luciano";
 myTuple[2] = false;
@@ -27,31 +27,45 @@ console.log(myTuple); // ['Luciano', 42, false]
 //objects
 let myObj;
 myObj = [];
-console.log(typeof (myObj));
+console.log(typeof myObj);
 myObj = {};
 const exampleObj = {
-    prop1: 'Davi',
-    prop2: true
+    prop1: "Davi",
+    prop2: true,
 };
 exampleObj.prop2 = false;
 let evh = {
-    name: 'Eddie',
+    name: "Eddie",
     active: false,
     albums: [1934, 234, "Ou32"],
 };
 let JP = {
-    name: 'Jimmy',
+    //   name: 'Jimmy',
     active: true,
-    albums: ["I", "II", 'IV'],
+    albums: ["I", "II", "IV"],
 };
 // https://www.youtube.com/watch?v=gieEQFIfgYc&t=17s 1:00:31
-//evh = JP // as soon  they have the same properties .. 
-// 
+//evh = JP // as soon  they have the same properties ..
+//
 //one property in an object can also be optional > use ?:
 //    active?: boolean,
-// using it in a function 
+// using it in a function
 console.clear();
 const greetGuitarist = (guitarist) => {
-    return `Hello ${guitarist.name}!`;
+    if (guitarist.name) {
+        return `Hello ${guitarist.name.toUpperCase()}!`;
+    }
+    return "Hello!";
 };
-console.log(greetGuitarist(JP));
+console.log(greetGuitarist(JP)); //Hello!
+console.log(greetGuitarist(evh)); //Hello EDDIE!
+// Enums : enumarated
+var Grade;
+(function (Grade) {
+    Grade[Grade["U"] = 1] = "U";
+    Grade[Grade["D"] = 2] = "D";
+    Grade[Grade["C"] = 3] = "C";
+    Grade[Grade["B"] = 4] = "B";
+    Grade[Grade["A"] = 5] = "A";
+})(Grade || (Grade = {}));
+console.log(Grade.U); // 0
--- src/main.ts
+++ src/main.ts
@@ -1,103 +1,109 @@
-let stringArr = ['one', 'hey', 'Dave']; // let stringArr: string[]
-let guitars = ['Strat', 'Les Paul', 5150]; // let guitars: (string | number)[]
-let mixedData = ['EVH', 1984, true]; // let mixedData: (string | number | boolean)[]
+let stringArr = ["one", "hey", "Dave"]; // let stringArr: string[]
+let guitars = ["Strat", "Les Paul", 5150]; // let guitars: (string | number)[]
+let mixedData = ["EVH", 1984, true]; // let mixedData: (string | number | boolean)[]
 
+stringArr[0] = "Luciano";
 
+stringArr.push("0");
 
+guitars.unshift(9);
 
- stringArr[0] = "Luciano";
-
-stringArr.push("0")
-
-guitars.unshift(9)
-
-console.log(guitars);//[9, 'Strat', 'Les Paul', 5150]
+console.log(guitars); //[9, 'Strat', 'Les Paul', 5150]
 console.log(stringArr); //['Luciano', 'hey', 'Dave', '0']
 
-
-
-
 // stringArr = mixedData;
-mixedData = stringArr
+mixedData = stringArr;
 
-
-// creating a any array type: 
+// creating a any array type:
 
 let test = [];
 
 // now a string array
-let bands: string[] = []
+let bands: string[] = [];
 
-bands.push("Van Halen")
+bands.push("Van Halen");
 
 // tuples
-// here you have to have the exactly order and length: 
-let myTuple: [string, number , boolean] = ['Dave', 42, true]
+// here you have to have the exactly order and length:
+let myTuple: [string, number, boolean] = ["Dave", 42, true];
 
-let mixed  = ['Dave', 42, true]
+let mixed = ["Dave", 42, true];
 
-mixed = myTuple // no problem here 
+mixed = myTuple; // no problem here
 
 //myTuple = mixed // not assignable
 
-myTuple[0] = "Luciano"
-myTuple[2] = false
+myTuple[0] = "Luciano";
+myTuple[2] = false;
 // myTuple[1]  ="2" // cannot be done
 
-console.log(myTuple);// ['Luciano', 42, false]
+console.log(myTuple); // ['Luciano', 42, false]
 
 //objects
 let myObj: object;
 myObj = [];
-console.log(typeof(myObj));
+console.log(typeof myObj);
 
 myObj = {};
 
 const exampleObj = {
-    prop1: 'Davi',
-    prop2: true
-}
+  prop1: "Davi",
+  prop2: true,
+};
 
 exampleObj.prop2 = false;
-// exampleObj.prop2 = "Luciano";// can't cause it is boolean 
-
+// exampleObj.prop2 = "Luciano";// can't cause it is boolean
 
 type Guitarist = {
-     // could use interface Guitarist { >>> use interface for classes(is better for classes)
-        // name....
-    //  }
-    name: string,
-    active?: boolean,
-    //one property in an object can also be optional > use ?:
-//    active?: boolean,
+  // could use interface Guitarist { >>> use interface for classes(is better for classes)
+  // name....
+  //  }
+  name?: string;
+  active: boolean;
+  //one property in an object can also be optional > use ?:
+  //    active?: boolean,
 
-    albums: (string | number )[],
-
-}
+  albums: (string | number)[];
+};
 let evh: Guitarist = {
-    name: 'Eddie',
-    active: false,
-    albums: [1934, 234, "Ou32"],
-}
+  name: "Eddie",
+  active: false,
+  albums: [1934, 234, "Ou32"],
+};
 
 let JP: Guitarist = {
-    name: 'Jimmy',
-    active: true,
-    albums: ["I", "II", 'IV'],
-}
+//   name: 'Jimmy',
+  active: true,
+  albums: ["I", "II", "IV"],
+};
 // https://www.youtube.com/watch?v=gieEQFIfgYc&t=17s 1:00:31
-//evh = JP // as soon  they have the same properties .. 
+//evh = JP // as soon  they have the same properties ..
 
-// 
+//
 
-  //one property in an object can also be optional > use ?:
+//one property in an object can also be optional > use ?:
 //    active?: boolean,
-// using it in a function 
+// using it in a function
 console.clear();
 
 const greetGuitarist = (guitarist: Guitarist) => {
-    return `Hello ${guitarist.name}!`;
+  if (guitarist.name) {
+    return `Hello ${guitarist.name.toUpperCase()}!`;
+  }
+  return "Hello!";
+};
+
+console.log(greetGuitarist(JP)); //Hello!
+
+console.log(greetGuitarist(evh));//Hello EDDIE!
+
+// Enums : enumarated
+
+enum Grade {
+    U =1,
+    D, // 1
+    C, //2
+    B, //3 
+    A,
 }
-
-console.log(greetGuitarist(JP));
-
+console.log(Grade.U); // 0
