0abb9bdef973d419dfd3c8ff2aa73637ed9f9681
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-08-04 21:01:00 -0300

    // getters and setters

--- build/js/main.js
+++ build/js/main.js
@@ -78,3 +78,27 @@ console.log(John.id); // 1
 console.log(Steve.id); // 2
 console.log(Amy.id); // 3
 /////////////////////////////////
+// getters and setters
+class Bands {
+    constructor() {
+        this.dataState = [];
+    }
+    get data() {
+        return this.dataState;
+    }
+    set data(value) {
+        if (Array.isArray(value) && value.every(el => typeof el === 'string')) {
+            this.dataState = value;
+            return;
+        }
+        else {
+            throw new Error('Param is not an array of strings');
+        }
+    }
+}
+const MyBands = new Bands();
+MyBands.data = ['Neil Young', 'Led Zep'];
+console.log(MyBands.data);
+MyBands.data = [...MyBands.data, 'ZZ Top'];
+console.log(MyBands.data);
+MyBands.data = ['Van Halen'];
--- src/main.ts
+++ src/main.ts
@@ -109,3 +109,36 @@ console.log(Steve.id); // 2
 console.log(Amy.id); // 3
 
 /////////////////////////////////
+
+// getters and setters
+
+class Bands {
+  private dataState: string[]
+  
+  constructor() {
+    this.dataState = []
+  }
+
+  public get data(): string[]{
+    return this.dataState
+  }
+
+  public set data(value: string[]) {
+    if(Array.isArray(value) && value.every(el => typeof el === 'string')){
+      this.dataState = value
+      return 
+    }else{
+      throw new Error('Param is not an array of strings')
+    }
+    }
+}
+
+
+const MyBands = new Bands() 
+MyBands.data = ['Neil Young', 'Led Zep']
+console.log(MyBands.data);
+MyBands.data = [...MyBands.data,'ZZ Top']
+
+console.log(MyBands.data);
+
+MyBands.data = ['Van Halen']
