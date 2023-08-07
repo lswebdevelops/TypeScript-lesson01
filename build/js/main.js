83664107d4620485540ef8d171c04e28202b4860
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-08-07 16:02:09 -0300

    // generics

--- build/js/main.js
+++ build/js/main.js
@@ -1,71 +1,140 @@
 "use strict";
-// index signature >>>
-// when we know the props' names
-// interface TransactionsObj {
-//   Pizza: number, 
-//   Books: number, 
-//   Job: number,
-// }
-const todaysTransactions = {
-    Pizza: -10,
-    Books: -5,
-    Job: 50,
+// generics
+// https://github.com/gitdagray/typescript-course/blob/main/lesson08/src/main.ts
+// sometimes we don't know what kind of types we are using 
+// const stringEcho = (arg: string): string => arg; >> this would just work with string 
+// we can use <and any letter here, usually T>
+const echo = (arg) => arg;
+//checking if it is an object 
+const isObj = (arg) => {
+    return (typeof arg === 'object' && !Array.isArray(arg) && arg !== null);
 };
-console.log(todaysTransactions.Pizza); // -10
-let prop = 'Pizza';
-console.log(todaysTransactions['prop']); //undefined
-const todaysNet = (transactions) => {
-    let total = 0;
-    for (const transaction in transactions) {
-        total += transactions[transaction];
+console.log(isObj(true)); // boolean> so false
+console.log(isObj("Davi")); // > string> false
+console.log(isObj([1, 2, 3])); // number array> false
+console.log(isObj({ name: 'John' })); // true >> it is an object
+console.log(isObj(null)); // null, so false
+const isTrue = (arg) => {
+    if (Array.isArray(arg) && !arg.length) {
+        return { arg, is: false };
     }
-    return total;
+    // if empty
+    if (isObj(arg) && !Object.keys(arg).length) {
+        return { arg, is: false };
+    }
+    //!!arg> returns a boolean : false or true
+    return { arg, is: !!arg };
 };
-console.log(todaysNet(todaysTransactions)); // 35
-const student = {
-    name: "Doug",
-    GPA: 3.5,
-    classes: [100, 200],
+console.log(isTrue(true)); //{arg: true, is: true}
+console.log(isTrue("Davi")); //{arg: 'Davi', is: true}
+console.log(isTrue([1, 2, 3])); //{arg: Array(3), is: true}
+console.log(isTrue({ name: 'John' })); //{arg: {…}, is: true}
+console.log(isTrue(null)); //{arg: null, is: false}
+console.log(isTrue([])); //{arg: Array(0), is: false}
+console.log(isTrue({})); //{arg: {...}, is: false}
+console.log(isTrue(1)); // true
+console.log(isTrue(0)); // false
+console.log(isTrue(-0)); // false
+const checkBoolValue = (arg) => {
+    if (Array.isArray(arg) && !arg.length) {
+        return { value: arg, is: false };
+    }
+    // if empty
+    if (isObj(arg) && !Object.keys(arg).length) {
+        return { value: arg, is: false };
+    }
+    //!!arg> returns a boolean : false or true
+    return { value: arg, is: !!arg };
 };
-// console.log(student.test);
-for (const key in student) {
-    console.log(`${key}: ${student[key]}`);
-}
-/*
-name: Doug
-main.js:33 GPA: 3.5
-main.js:33 classes: 100,200
-
-*/
-// or > using map 
-Object.keys(student).map(key => {
-    console.log(student[key]);
-});
-/*
-Doug
-VM383 main.js:43 3.5
-VM383 main.js:43 (2) [100, 200]
-*/
-// 2:54 ,min
-const logStudentKey = (student, key) => {
-    console.log(`Student ${key}: ${student[key]}`);
-};
-logStudentKey(student, 'GPA'); // Student GPA: 3.5
-logStudentKey(student, 'classes'); // Student GPA: 100, 200
-logStudentKey(student, 'name'); // Student GPA: Doug
-const monthlyIncomes = {
-    salary: 500,
-    bonus: 100,
-    sidehustle: 250,
-};
+console.log(checkBoolValue(true)); //{arg: true, is: true}
+console.log(checkBoolValue("Davi")); //{arg: 'Davi', is: true}
+console.log(checkBoolValue([1, 2, 3])); //{arg: Array(3), is: true}
+console.log(checkBoolValue({ name: 'John' })); //{arg: {…}, is: true}
+console.log(checkBoolValue(null)); //{arg: null, is: false}
+console.log(checkBoolValue([])); //{arg: Array(0), is: false}
+console.log(checkBoolValue({})); //{arg: {...}, is: false}
+console.log(checkBoolValue(1)); // true
+console.log(checkBoolValue(0)); // false
+console.log(checkBoolValue(-0)); // false
 console.clear();
-for (const Revenue in monthlyIncomes) {
-    console.log(monthlyIncomes[Revenue]);
+console.clear();
+const processUser = (user) => {
+    // process the user with logic here 
+    return user;
+};
+console.log(processUser({ id: 1, name: 'Dave' }));
+console.log(processUser({ id: 2, name: 'Dave M' }));
+// console.log(processUser({ name: 'Dave J'}));  brings an error
+const getUsersProperty = (users, key) => {
+    return users.map(user => user[key]);
+};
+const usersArray = [
+    {
+        "id": 1,
+        "name": "Leanne Graham",
+        "username": "Bret",
+        "email": "Sincere@april.biz",
+        "address": {
+            "street": "Kulas Light",
+            "suite": "Apt. 556",
+            "city": "Gwenborough",
+            "zipcode": "92998-3874",
+            "geo": {
+                "lat": "-37.3159",
+                "lng": "81.1496"
+            }
+        },
+        "phone": "1-770-736-8031 x56442",
+        "website": "hildegard.org",
+        "company": {
+            "name": "Romaguera-Crona",
+            "catchPhrase": "Multi-layered client-server neural-net",
+            "bs": "harness real-time e-markets"
+        }
+    },
+    {
+        "id": 2,
+        "name": "Ervin Howell",
+        "username": "Antonette",
+        "email": "Shanna@melissa.tv",
+        "address": {
+            "street": "Victor Plains",
+            "suite": "Suite 879",
+            "city": "Wisokyburgh",
+            "zipcode": "90566-7771",
+            "geo": {
+                "lat": "-43.9509",
+                "lng": "-34.4618"
+            }
+        },
+        "phone": "010-692-6593 x09125",
+        "website": "anastasia.net",
+        "company": {
+            "name": "Deckow-Crist",
+            "catchPhrase": "Proactive didactic contingency",
+            "bs": "synergize scalable supply-chains"
+        }
+    },
+];
+console.clear();
+console.log(getUsersProperty(usersArray, "email")); //['Sincere@april.biz', 'Shanna@melissa.tv']
+console.log(getUsersProperty(usersArray, "username")); // (2) ['Bret', 'Antonette']
+class StateObject {
+    constructor(value) {
+        this.data = value;
+    }
+    get state() {
+        return this.data;
+    }
+    set state(value) {
+        this.data = value;
+    }
 }
-/*
-
-500
-100
-250
-
-*/
+console.clear();
+const store = new StateObject("John");
+console.log(store.state); // John
+store.state = "Dave";
+console.log(store.state); // Dave
+//  store.state = 12// error>> string it should be
+const myState = new StateObject([15]);
+myState.state = (["Dave", 42, true]);
--- src/main.ts
+++ src/main.ts
@@ -1,117 +1,174 @@
-// index signature >>>
+// generics
+// https://github.com/gitdagray/typescript-course/blob/main/lesson08/src/main.ts
 
-interface TransactionsObj {
-  [index: string]: number
-     Pizza: number, 
-    Books: number, 
-    Job: number,
-}
- 
-// when we know the props' names
-// interface TransactionsObj {
-  //   Pizza: number, 
-  //   Books: number, 
-  //   Job: number,
-  // }
+// sometimes we don't know what kind of types we are using 
+
+// const stringEcho = (arg: string): string => arg; >> this would just work with string 
+// we can use <and any letter here, usually T>
+ const echo = <T>(arg: T): T => arg;
+
+ //checking if it is an object 
+ const isObj = <T>(arg: T): boolean => {
+  return (typeof arg === 'object' && !Array.isArray
+  (arg) && arg !==null)
+ }
+
+ console.log(isObj(true)); // boolean> so false
+ console.log(isObj("Davi")); // > string> false
+ console.log(isObj([1,2,3])); // number array> false
+ console.log(isObj({name: 'John'})); // true >> it is an object
+ console.log(isObj(null)); // null, so false
+
+
+const isTrue = <T>(arg: T): {arg: T, is: boolean} => {
   
-const todaysTransactions: TransactionsObj = {
-  Pizza: -10,
-  Books: -5,
-  Job: 50,
-
-}
-console.log(todaysTransactions.Pizza); // -10
-
-let prop: string = 'Pizza'
-
-console.log(todaysTransactions['prop']); //undefined
-
-const todaysNet  = (transactions: TransactionsObj):
-number => {
-  let total = 0
-  for (const transaction in transactions) {
-    total += transactions[transaction]
+  if(Array.isArray(arg) && !arg.length) {
+    return{ arg, is: false}
   }
-  return total;
+  // if empty
+  if(isObj(arg) && !Object.keys(arg as keyof T).length){
+    return { arg, is: false}
+  }
+    //!!arg> returns a boolean : false or true
+  return { arg, is: !!arg}
 }
 
-console.log(todaysNet(todaysTransactions));// 35
-/////////////////////////////
+console.log(isTrue(true)); //{arg: true, is: true}
+console.log(isTrue("Davi"));//{arg: 'Davi', is: true}
+console.log(isTrue([1,2,3]));//{arg: Array(3), is: true}
+console.log(isTrue({name: 'John'})); //{arg: {…}, is: true}
+console.log(isTrue(null)); //{arg: null, is: false}
+console.log(isTrue([]));//{arg: Array(0), is: false}
+console.log(isTrue({}));//{arg: {...}, is: false}
+console.log(isTrue(1));// true
+console.log(isTrue(0));// false
+console.log(isTrue(-0));// false
 
-interface Student {
-  // [key: string] : string | number | number[]| undefined
-  name: string,
-  GPA: number,
-  classes?: number[],
-
+// rewriting the function using interface: 
+interface BoolCheck<T> {
+  value: T,
+  is: boolean,
 }
-
-const student: Student = {
-  name: "Doug",
-  GPA: 3.5,
-  classes: [100,200],
-
-}
-
-// console.log(student.test);
-for (const key in student) {
-  console.log(`${key}: ${student[key as keyof Student]}`);
-}
-
-/*
-name: Doug
-main.js:33 GPA: 3.5
-main.js:33 classes: 100,200
-
-*/
-
-// or > using map 
-
-Object.keys(student).map(key => {
-  console.log(student[key as keyof typeof student]);
-})
-/*
-Doug
-VM383 main.js:43 3.5
-VM383 main.js:43 (2) [100, 200]
-*/
-// 2:54 ,min
-
-const logStudentKey = (student: Student, key: keyof Student): void => {
-  console.log(`Student ${key}: ${student[key]}`);
-}
-
-logStudentKey(student, 'GPA')// Student GPA: 3.5
-logStudentKey(student, 'classes')// Student GPA: 100, 200
-logStudentKey(student, 'name')// Student GPA: Doug
-
-/////////////////////////////
-
-// interface Incomes {
-  //   [key: string]: number
+const checkBoolValue = <T>(arg: T): BoolCheck<T> => {
   
-  // }
-
-type Streams = 'salary'| 'bonus' | 'sidehustle'
-
-type Incomes = Record<Streams, number>
-
-const monthlyIncomes: Incomes = {
-  salary: 500,
-  bonus: 100,
-  sidehustle: 250,
+  if(Array.isArray(arg) && !arg.length) {
+    return{ value:arg, is: false}
+  }
+  // if empty
+  if(isObj(arg) && !Object.keys(arg as keyof T).length){
+    return { value:arg, is: false}
+  }
+    //!!arg> returns a boolean : false or true
+  return {value:arg, is: !!arg}
 }
 
+
+console.log(checkBoolValue(true)); //{arg: true, is: true}
+console.log(checkBoolValue("Davi"));//{arg: 'Davi', is: true}
+console.log(checkBoolValue([1,2,3]));//{arg: Array(3), is: true}
+console.log(checkBoolValue({name: 'John'})); //{arg: {…}, is: true}
+console.log(checkBoolValue(null)); //{arg: null, is: false}
+console.log(checkBoolValue([]));//{arg: Array(0), is: false}
+console.log(checkBoolValue({}));//{arg: {...}, is: false}
+console.log(checkBoolValue(1));// true
+console.log(checkBoolValue(0));// false
+console.log(checkBoolValue(-0));// false
 console.clear();
 
-for (const Revenue in monthlyIncomes){ 
-  console.log(monthlyIncomes[Revenue as keyof Incomes]);
+interface HasId {
+  id: number,
+}
+console.clear();
+
+const processUser = <T extends HasId>(user: T): T => {
+  // process the user with logic here 
+  return user
 }
 
-/*
+console.log(processUser({id:1, name: 'Dave'}));
+console.log(processUser({id:2, name: 'Dave M'}));
+// console.log(processUser({ name: 'Dave J'}));  brings an error
 
-500
-100
-250
 
-*/
+
+const getUsersProperty = <T extends HasId, K extends keyof T>(users: T[], key: K): T[K][] => {
+  return users.map(user => user[key])
+}
+
+const usersArray = [
+  {
+      "id": 1,
+      "name": "Leanne Graham",
+      "username": "Bret",
+      "email": "Sincere@april.biz",
+      "address": {
+          "street": "Kulas Light",
+          "suite": "Apt. 556",
+          "city": "Gwenborough",
+          "zipcode": "92998-3874",
+          "geo": {
+              "lat": "-37.3159",
+              "lng": "81.1496"
+          }
+      },
+      "phone": "1-770-736-8031 x56442",
+      "website": "hildegard.org",
+      "company": {
+          "name": "Romaguera-Crona",
+          "catchPhrase": "Multi-layered client-server neural-net",
+          "bs": "harness real-time e-markets"
+      }
+  },
+  {
+      "id": 2,
+      "name": "Ervin Howell",
+      "username": "Antonette",
+      "email": "Shanna@melissa.tv",
+      "address": {
+          "street": "Victor Plains",
+          "suite": "Suite 879",
+          "city": "Wisokyburgh",
+          "zipcode": "90566-7771",
+          "geo": {
+              "lat": "-43.9509",
+              "lng": "-34.4618"
+          }
+      },
+      "phone": "010-692-6593 x09125",
+      "website": "anastasia.net",
+      "company": {
+          "name": "Deckow-Crist",
+          "catchPhrase": "Proactive didactic contingency",
+          "bs": "synergize scalable supply-chains"
+      }
+  },
+]
+
+console.clear();
+console.log(getUsersProperty(usersArray, "email"));//['Sincere@april.biz', 'Shanna@melissa.tv']
+console.log(getUsersProperty(usersArray, "username"));// (2) ['Bret', 'Antonette']
+
+class StateObject<T> {
+  private data: T
+
+  constructor(value: T) {
+    this.data = value;
+  }
+  get state(): T {
+    return this.data
+  }
+  set state(value: T) {
+    this.data = value;
+  }
+}
+console.clear();
+const store = new StateObject("John")
+console.log(store.state); // John
+store.state = "Dave" 
+console.log(store.state); // Dave
+//  store.state = 12// error>> string it should be
+
+const myState = new StateObject<(string | number| boolean)[]>([15]) 
+myState.state = (["Dave", 42 , true])
+
+
